602b1ec48a9611029c814ef09551efc71ae3e38c
lushun <1720920833@qq.com>
2019-10-14 15:47:44 +0800

    广告页面

--- app/src/main/java/com/ls/rjianzye/activity/AdymActivity.java
+++ app/src/main/java/com/ls/rjianzye/activity/AdymActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -12,38 +13,37 @@ import com.ls.rjianzye.R;
 public class AdymActivity extends AppCompatActivity {
     TextView tv_ad;
 
-    Handler handler=new Handler();
-
+    Handler handler = new Handler();
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adym);
-        tv_ad = findViewById(R.id.tv_ad);
 
+        findViewById(R.id.tv_ad);
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i=5;i>=0;i--){
-                    SystemClock.sleep(1000);
-                    final int count=i;
-
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            tv_ad.setText("点击转跳 "+count);
-                        }
-                    }) ;
-                }
+                int i;
+                for (i = 5; i >= 5; i--) ;
+                SystemClock.sleep(1000);
+                final int count = i;
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
-                        Intent intent = new Intent(AdymActivity.this,MainActivity.class);
-                        startActivity(intent);
+                        tv_ad.setText("点击转跳 "+count);
 
-                        finish();
                     }
                 });
+               runOnUiThread(new Runnable() {
+                   @Override
+                   public void run() {
+                       Intent intent1 = new Intent(AdymActivity.this, MainActivity.class);
+                       startActivity(intent1);
+
+                       finish();
+                   }
+               });
             }
         }).start();
     }
