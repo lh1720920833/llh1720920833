66f6e2b61cab32b098180ee969dfc26e6a038eec
huatec-1 <1720920833@qq.com>
2019-10-15 11:11:29 +0800

    广告页面2

--- app/src/main/java/com/ls/rjianzye/activity/AdymActivity.java
+++ app/src/main/java/com/ls/rjianzye/activity/AdymActivity.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.ls.rjianzye.R;
@@ -19,22 +20,25 @@ public class AdymActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adym);
 
-        findViewById(R.id.tv_ad);
+        tv_ad=findViewById(R.id.tv_ad);
+
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 int i;
-                for (i = 5; i >= 5; i--) ;
-                SystemClock.sleep(1000);
-                final int count = i;
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        tv_ad.setText("点击转跳 "+count);
+                for (i = 5; i >= 0; i--) {
+                    SystemClock.sleep(1000);
+                    final int count = i;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv_ad.setText("点击转跳 " + count);
 
-                    }
-                });
+                        }
+                    });
+
+                }
                runOnUiThread(new Runnable() {
                    @Override
                    public void run() {
