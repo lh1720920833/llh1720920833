bb33d3c901c4b78ac28b93c028411587cbc10939
lushun <1720920833@qq.com>
2019-12-02 22:58:00 +0800

    分类

--- app/src/main/java/com/ls/rjianzye/fragment/CategoryFragment.java
+++ app/src/main/java/com/ls/rjianzye/fragment/CategoryFragment.java
@@ -7,14 +7,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ls.rjianzye.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 public class CategoryFragment extends Fragment {
+    @BindView(R.id.tv_gou)
+    TextView tvGou;
+    Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+        unbinder = ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.tv_gou)
+    public void onViewClicked() {
     }
 }
